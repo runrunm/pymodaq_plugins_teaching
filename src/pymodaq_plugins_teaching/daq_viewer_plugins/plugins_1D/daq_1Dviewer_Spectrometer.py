56a3a1e15993eedb39acdb13753689861d53e1fc
Sébastien Weber <seba.weber@gmail.com>
2022-10-10 17:05:51 +0200

    adapted plugins to simpler templates

--- src/pymodaq_plugins_teaching/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Spectrometer.py
+++ src/pymodaq_plugins_teaching/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Spectrometer.py
@@ -25,10 +25,15 @@ class DAQ_1DViewer_Spectrometer(DAQ_Viewer_base):
     params = comon_parameters +\
              [{'title': 'Grating:', 'name': 'grating', 'type': 'list', 'values': Spectrometer.gratings}]
 
-    def commit_settings(self, param):
-        """
-        """
 
+    def commit_settings(self, param):
+        """Apply the consequences of a change of value in the detector settings
+
+        Parameters
+        ----------
+        param: Parameter
+            A given parameter (within detector_settings) whose value has been changed by the user
+        """
         if param.name() == 'grating':
            self.controller.grating = param.value()
 
@@ -37,71 +42,58 @@ class DAQ_1DViewer_Spectrometer(DAQ_Viewer_base):
 
         Parameters
         ----------
-        controller: (object) custom object of a PyMoDAQ plugin (Slave case). None if only one detector by controller (Master case)
+        controller: (object)
+            custom object of a PyMoDAQ plugin (Slave case). None if only one actuator/detector by controller
+            (Master case)
 
         Returns
         -------
-        self.status (edict): with initialization status: three fields:
-            * info (str)
-            * controller (object) initialized controller
-            *initialized: (bool): False if initialization failed otherwise True
+        info: str
+        initialized: bool
+            False if initialization failed otherwise True
         """
 
-        try:
-            self.status.update(edict(initialized=False,info="",x_axis=None,y_axis=None,controller=None))
-            if self.settings.child(('controller_status')).value() == "Slave":
-                if controller is None:
-                    raise Exception('no controller has been defined externally while this detector is a slave one')
-                else:
-                    self.controller = controller
-            else:
-                self.controller = Spectrometer()
-                self.controller.open_communication()
-                #####################################
+        self.controller = self.ini_detector_init(old_controller=controller,
+                                                 new_controller=Spectrometer())
+        self.controller.open_communication()
 
-            #initialize viewers pannel with the future type of data
-            self.data_grabed_signal_temp.emit([DataFromPlugins(name='Monochromator',
-                                                               data=[np.zeros((Spectrometer.Nx, ))],
-                                                               dim='Data1D',
-                                                               labels=['Spectrometer Amplitudes'])])
+        #initialize viewers pannel with the future type of data
+        self.data_grabed_signal_temp.emit([DataFromPlugins(name='Monochromator',
+                                                           data=[np.zeros((Spectrometer.Nx, ))],
+                                                           dim='Data1D',
+                                                           labels=['Spectrometer Amplitudes'])])
+        self.emit_x_axis(Axis(data=self.controller.get_wavelength_axis(),
+                              label='Wavelength',
+                              units='nm'))
 
-            self.status.info = Spectrometer.infos
-            self.status.initialized = True
-            self.status.controller = self.controller
-            return self.status
-
-        except Exception as e:
-            self.emit_status(ThreadCommand('Update_Status', [getLineInfo() + str(e), 'log']))
-            self.status.info = getLineInfo() + str(e)
-            self.status.initialized = False
-            return self.status
+        info = Spectrometer.infos
+        initialized = True
+        return info, initialized
 
     def close(self):
-        """
-        Terminate the communication protocol
-        """
+        """Terminate the communication protocol"""
         self.controller.close_communication()
 
     def grab_data(self, Naverage=1, **kwargs):
-        """
+        """Start a grab from the detector
 
         Parameters
         ----------
-        Naverage: (int) Number of hardware averaging
-        kwargs: (dict) of others optionals arguments
+        Naverage: int
+            Number of hardware averaging (if hardware averaging is possible, self.hardware_averaging should be set to
+            True in class preamble and you should code this implementation)
+        kwargs: dict
+            others optionals arguments
         """
         data_tot = self.controller.grab_spectrum()
         self.data_grabed_signal.emit([DataFromPlugins(name='Monochromator',
                                                       data=[data_tot],
                                                       dim='Data1D',
-                                                      labels=['Spectrometer Amplitudes'],
-                                                      x_axis=Axis(data=self.controller.get_wavelength_axis(),
-                                                                  label='Wavelength',
-                                                                  units='nm'))])
+                                                      labels=['Spectrometer Amplitudes']
+                                                      )])
 
     def stop(self):
         self.controller.stop()
-
         return ''
 
 

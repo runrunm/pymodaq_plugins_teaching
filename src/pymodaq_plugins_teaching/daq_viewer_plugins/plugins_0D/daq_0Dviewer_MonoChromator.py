d95caa7c3be15fdd6dadaf8317496a49cdbcc37c
Sébastien Weber <seba.weber@gmail.com>
2023-11-08 14:16:07 +0100

    modified mono

--- src/pymodaq_plugins_teaching/daq_viewer_plugins/plugins_0D/daq_0Dviewer_MonoChromator.py
+++ src/pymodaq_plugins_teaching/daq_viewer_plugins/plugins_0D/daq_0Dviewer_MonoChromator.py
@@ -1,28 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created the 09/10/2022
-
-@author: Sebastien Weber
-"""
-
 import numpy as np
-from easydict import EasyDict as edict
-from pymodaq.utils.daq_utils import ThreadCommand, getLineInfo, DataFromPlugins
+from pymodaq.utils.daq_utils import ThreadCommand
+from pymodaq.utils.data import DataFromPlugins, DataToExport
 from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters, main
+from pymodaq.utils.parameter import Parameter
+
 
 from pymodaq_plugins_teaching.hardware.spectrometer import Spectrometer
 
 
-class DAQ_0DViewer_Monochromator(DAQ_Viewer_base):
+class DAQ_0DViewer_MonoChromator(DAQ_Viewer_base):
     """
     """
-    params = comon_parameters +\
-             []
+    params = comon_parameters+[
+        ]
 
     def ini_attributes(self):
-        self.controller = None
+        self.controller: Spectrometer = None
 
-    def commit_settings(self, param):
+    def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings
 
         Parameters
@@ -47,19 +42,16 @@ class DAQ_0DViewer_Monochromator(DAQ_Viewer_base):
         initialized: bool
             False if initialization failed otherwise True
         """
-        self.controller = self.ini_detector_init(old_controller=controller,
-                                                 new_controller=Spectrometer())
-        self.controller.open_communication()
 
+        self.ini_detector_init(old_controller=controller,
+                               new_controller=Spectrometer())
 
-        #initialize viewers pannel with the future type of data
-        self.data_grabed_signal_temp.emit([DataFromPlugins(name='Monochromator',
-                                                           data=[np.array([0.])],
-                                                           dim='Data0D',
-                                                           labels=['MonoChromator Amplitude'])])
+        if self.settings['multiaxes', 'multi_status'] == 'Master':
+            initialized = self.controller.open_communication()
+        else:
+            initialized = True
 
-        info = Spectrometer.infos
-        initialized = True
+        info = "Whatever info you want to log"
         return info, initialized
 
     def close(self):
@@ -78,14 +70,16 @@ class DAQ_0DViewer_Monochromator(DAQ_Viewer_base):
             others optionals arguments
         """
         data_tot = self.controller.grab_monochromator()
-        self.data_grabed_signal.emit([DataFromPlugins(name='Monochromator',
-                                                      data=[data_tot],
-                                                      dim='Data0D',
-                                                      labels=['MonoChromator Amplitude'])])
+        self.dte_signal.emit(
+            DataToExport(name='myplugin',
+                         data=[DataFromPlugins(name='Mock1', data=[data_tot],
+                                               dim='Data0D', labels=['photodiode'])]))
 
     def stop(self):
+        """Stop the current grab hardware wise if necessary"""
         self.controller.stop()
-
+        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        ##############################
         return ''
 
 

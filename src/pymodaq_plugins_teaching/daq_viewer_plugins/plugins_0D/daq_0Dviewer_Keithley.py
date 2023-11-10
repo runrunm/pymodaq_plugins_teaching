22b06d4e1222cb2e48fce78d245b6adbdf875d22
Sébastien Weber <seba.weber@gmail.com>
2023-11-10 10:16:40 +0100

    Update daq_0Dviewer_Keithley.py

--- src/pymodaq_plugins_teaching/daq_viewer_plugins/plugins_0D/daq_0Dviewer_Keithley.py
+++ src/pymodaq_plugins_teaching/daq_viewer_plugins/plugins_0D/daq_0Dviewer_Keithley.py
@@ -3,12 +3,12 @@ from pymodaq.utils.daq_utils import ThreadCommand
 from pymodaq.utils.data import DataFromPlugins, DataToExport
 from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters, main
 from pymodaq.utils.parameter import Parameter
-
+from pymodaq.utils.parameter import utils as putils
 if True:
-    from pylablib.devices.Keithley.multimeter import Keithley2110
+    from pylablib.devices.Keithley.multimeter import Keithley2110, TGenericFunctionParameters
     from pyvisa import ResourceManager
 else:
-    from pymodaq_plugins_teaching.hardware.keithley import Keithley2110, ResourceManager
+    from pymodaq_plugins_teaching.hardware.keithley import Keithley2110, ResourceManager, TGenericFunctionParameters
 
 rm = ResourceManager()
 
@@ -21,9 +21,13 @@ class DAQ_0DViewer_Keithley(DAQ_Viewer_base):
     """
     params = comon_parameters+[
         {'title': 'Connections:', 'name': 'addresses', 'type': 'list', 'limits': RESOURCES},
+        {'title': 'Info:', 'name': 'info', 'type': 'str',},
         {'title': 'Measurements:', 'name': 'measure', 'type': 'list', 'limits': MEASUREMENTS},
         {'title': 'Config:', 'name': 'config', 'type': 'group', 'children': [
-
+            {'title': 'Reset:', 'name': 'reset', 'type': 'bool_push', 'value': False},
+            {'title': 'Autorange:', 'name': 'auto', 'type': 'bool', 'value': True},
+            {'title': 'Range:', 'name': 'range', 'type': 'float'},
+            {'title': 'Resolution:', 'name': 'resolution', 'type': 'float'},
 
         ]},
         ]
@@ -41,6 +45,16 @@ class DAQ_0DViewer_Keithley(DAQ_Viewer_base):
         """
         if param.name() == 'measure':
             self.controller.set_function(param.value())
+            self.get_params()
+
+        elif param.name() == 'auto':
+            self.set_params('autorng', param.value())
+        elif param.name() == 'range':
+            self.set_params('rng', param.value())
+        elif param.name() == 'resolution':
+            self.set_params('resolution', param.value())
+        elif param.name() == 'reset':
+            self.controller.reset()
 
     def ini_detector(self, controller=None):
         """Detector communication initialization
@@ -61,9 +75,27 @@ class DAQ_0DViewer_Keithley(DAQ_Viewer_base):
         self.ini_detector_init(old_controller=controller,
                                new_controller=Keithley2110(self.settings['addresses']))
 
+        info = self.controller.get_id()
+        self.settings.child('info').setValue(info)
+        measurement = self.controller.get_function('primary')
+        self.settings.child('measure').setValue(measurement)
+        self.get_params()
+
         initialized = True
-        info = "Whatever info you want to log"
         return info, initialized
+
+    def get_params(self):
+        measurement = self.controller.get_function('primary')
+        status = self.controller.get_function_parameters(measurement)
+
+    def _write_params(self, params: TGenericFunctionParameters):
+        self.settings.child('config', 'auto').setValue(params.autorng)
+        self.settings.child('config', 'range').setValue(params.rng)
+        self.settings.child('config', 'resolution').setValue(params.resolution)
+
+    def set_params(self, param_name, param_value):
+        self._write_params(self.controller.set_function_parameters(self.controller.get_function('primary'),
+                                                                   **{param_name: param_value}))
 
     def close(self):
         """Terminate the communication protocol"""

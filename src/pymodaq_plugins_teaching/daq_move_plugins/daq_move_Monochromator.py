5e58d008150e521b228e768dc353bf7f38341f5a
Sébastien Weber <seba.weber@gmail.com>
2024-06-18 21:20:49 +0200

    properly close the communication channel

--- src/pymodaq_plugins_teaching/daq_move_plugins/daq_move_Monochromator.py
+++ src/pymodaq_plugins_teaching/daq_move_plugins/daq_move_Monochromator.py
@@ -61,9 +61,7 @@ class DAQ_Move_Monochromator(DAQ_Move_base):
 
     def close(self):
         """Terminate the communication protocol"""
-        ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        #  self.controller.your_method_to_terminate_the_communication()  # when writing your own plugin replace this line
+        self.controller.close_communication()  # when writing your own plugin replace this line
 
     def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings

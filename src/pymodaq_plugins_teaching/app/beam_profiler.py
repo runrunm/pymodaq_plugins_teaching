20c8b8055ca74f4fa34d29bdbb9b8a6151cfaff5
Sébastien Weber <seba.weber@gmail.com>
2024-06-20 21:56:24 +0200

    renaming template with appropriate name

--- src/pymodaq_plugins_teaching/app/beam_profiler.py
+++ src/pymodaq_plugins_teaching/app/beam_profiler.py
@@ -5,7 +5,7 @@ from pymodaq.utils.config import Config
 from pymodaq.utils.logger import set_logger, get_module_name
 
 # todo: replace here *pymodaq_plugins_template* by your plugin package name
-from pymodaq_plugins_template.utils import Config as PluginConfig
+from pymodaq_plugins_teaching.utils import Config as PluginConfig
 
 logger = set_logger(get_module_name(__file__))
 
@@ -15,7 +15,7 @@ plugin_config = PluginConfig()
 
 # todo: modify the name of this class to reflect its application and change the name in the main
 # method at the end of the script
-class CustomAppTemplate(gutils.CustomApp):
+class BeamProfiler(gutils.CustomApp):
 
     # todo: if you wish to create custom Parameter and corresponding widgets. These will be
     # automatically added as children of self.settings. Morevover, the self.settings_tree will
@@ -114,7 +114,7 @@ def main():
     mainwindow.setCentralWidget(dockarea)
 
     # todo: change the name here to be the same as your app class
-    prog = CustomAppTemplate(dockarea)
+    prog = BeamProfiler(dockarea)
 
     mainwindow.show()
 

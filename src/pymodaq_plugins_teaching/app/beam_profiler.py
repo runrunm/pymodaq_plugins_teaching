ad899a739ea097a938d76679b3a3f0fe5eef8e4c
Sébastien Weber <seba.weber@gmail.com>
2024-06-20 22:22:31 +0200

    adding 2 actions with proper signature

--- src/pymodaq_plugins_teaching/app/beam_profiler.py
+++ src/pymodaq_plugins_teaching/app/beam_profiler.py
@@ -68,24 +68,16 @@ class BeamProfiler(gutils.CustomApp):
 
     def setup_actions(self):
         """Method where to create actions to be subclassed. Mandatory
-
-        Examples
-        --------
-        >>> self.add_action('Quit', 'close2', "Quit program")
-        >>> self.add_action('Grab', 'camera', "Grab from camera", checkable=True)
-        >>> self.add_action('Load', 'Open', "Load target file (.h5, .png, .jpg) or data from camera"
-            , checkable=False)
-        >>> self.add_action('Save', 'SaveAs', "Save current data", checkable=False)
-
-        See Also
-        --------
-        ActionManager.add_action
         """
-        self.add_action('Grab', 'camera', "Grab from camera", checkable=True)
-        self.add_action('Quit', 'close2', "Quit program")
-        
+        self.add_action('grab', 'Grab', 'camera', "Grab from camera", checkable=True)
+        self.add_action('quit', 'Quit', 'close2', "Quit program")
+
     def connect_things(self):
         """Connect actions and/or other widgets signal to methods"""
+        self.connect_action('grab', self.camera_viewer.grab)
+        self.connect_action('quit', self.quit_app)
+
+    def quit_app(self):
         pass
 
     def setup_menu(self):

c94edecd901681f6546eb184faeb8df8dc7830a0
Sébastien Weber <seba.weber@gmail.com>
2025-06-22 10:03:50 +0200

    copying the 0D file and renamed it with 1D Spectrometer

---
+++ src/pymodaq_plugins_teaching/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Spectrometer.py
@@ -0,0 +1,111 @@
+import numpy as np
+from pymodaq.utils.daq_utils import ThreadCommand
+from pymodaq.utils.data import DataFromPlugins, DataToExport
+from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters, main
+from pymodaq.utils.parameter import Parameter
+
+from pymodaq_plugins_teaching.hardware.spectrometer import Spectrometer
+
+
+class DAQ_0DViewer_MonochromatorPhotodiode(DAQ_Viewer_base):
+    """ Instrument plugin class for a OD viewer.
+    
+    This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Viewer module through inheritance via
+    DAQ_Viewer_base. It makes a bridge between the DAQ_Viewer module and the Python wrapper of a particular instrument.
+
+    TODO Complete the docstring of your plugin with:
+        * The set of instruments that should be compatible with this instrument plugin.
+        * With which instrument it has actually been tested.
+        * The version of PyMoDAQ during the test.
+        * The version of the operating system.
+        * Installation instructions: what manufacturer’s drivers should be installed to make it run?
+
+    Attributes:
+    -----------
+    controller: object
+        The particular object that allow the communication with the hardware, in general a python wrapper around the
+         hardware library.
+         
+    # TODO add your particular attributes here if any
+
+    """
+    params = comon_parameters+[
+        ## TODO for your custom plugin: elements to be added here as dicts in order to control your custom stage
+        ]
+
+    def ini_attributes(self):
+        self.controller: Spectrometer = None
+
+    def commit_settings(self, param: Parameter):
+        """Apply the consequences of a change of value in the detector settings
+
+        Parameters
+        ----------
+        param: Parameter
+            A given parameter (within detector_settings) whose value has been changed by the user
+        """
+        ## TODO for your custom plugin
+        if param.name() == "a_parameter_you've_added_in_self.params":
+           self.controller.your_method_to_apply_this_param_change()  # when writing your own plugin replace this line
+#        elif ...
+        ##
+
+    def ini_detector(self, controller=None):
+        """Detector communication initialization
+
+        Parameters
+        ----------
+        controller: (object)
+            custom object of a PyMoDAQ plugin (Slave case). None if only one actuator/detector by controller
+            (Master case)
+
+        Returns
+        -------
+        info: str
+        initialized: bool
+            False if initialization failed otherwise True
+        """
+
+        self.ini_detector_init(old_controller=controller,
+                               new_controller=Spectrometer())
+        if self.is_master:
+            initialized = self.controller.open_communication()
+        else:
+            initialized = True
+
+        info = "Whatever info you want to log"
+        return info, initialized
+
+    def close(self):
+        """Terminate the communication protocol"""
+        if self.is_master:
+            self.controller.close_communication()  # when writing your own plugin replace this line
+
+    def grab_data(self, Naverage=1, **kwargs):
+        """Start a grab from the detector
+
+        Parameters
+        ----------
+        Naverage: int
+            Number of hardware averaging (if hardware averaging is possible, self.hardware_averaging should be set to
+            True in class preamble and you should code this implementation)
+        kwargs: dict
+            others optionals arguments
+        """
+        data_array = self.controller.grab_monochromator()
+        self.dte_signal.emit(DataToExport(name='MyMonochromator',
+                                          data=[DataFromPlugins(name='Photodiode',
+                                                                data=[data_array],
+                                                                dim='Data0D',
+                                                                labels=['Photodiode'])]))
+
+    def stop(self):
+        """Stop the current grab hardware wise if necessary"""
+        self.emit_status(ThreadCommand('Update_Status', ['no stopping available, grabbing is '
+                                                         'currently a blocking function']))
+        ##############################
+        return ''
+
+
+if __name__ == '__main__':
+    main(__file__)

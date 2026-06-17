1897120b53629da89f73d71c6febc6c8c5185714
bricolus <bbb@bricolus.fr>
2026-06-18 00:22:43 +0200

    Add beam profiler custom  DAQ 2D viewer

---
+++ src/pymodaq_plugins_teaching/daq_viewer_plugins/plugins_2D/daq_2Dviewer_BeamProfiler.py
@@ -0,0 +1,32 @@
+import numpy as np
+import laserbeamsize as lbs
+
+from pymodaq.control_modules.viewer_utility_classes import main
+
+# from pymodaq_plugins_mock.daq_viewer_plugins.plugins_2D.daq_2Dviewer_Mock import DAQ_2DViewer_Mock
+from pymodaq_plugins_mockexamples.daq_viewer_plugins.plugins_2D.daq_2Dviewer_BSCamera import DAQ_2DViewer_BSCamera
+
+from pymodaq_data.data import DataToExport, DataCalculated
+from pymodaq.utils.data import DataFromPlugins
+
+class DAQ_2DViewer_BeamProfiler(DAQ_2DViewer_BSCamera):
+    def grab_data(self, Naverage=1, **kwargs):
+        data = self.average_data(Naverage)
+
+        data_array = data.get_data_from_name('BSCamera')[0]
+
+        # Calculation
+        x, y, d_major, d_minor, phi = lbs.beam_size(data_array)
+
+        data_0d_diameter = DataCalculated('diameter', data=[np.atleast_1d(d_major),
+                                                            np.atleast_1d(d_minor)],
+                                                      labels=["Minor diameter", "Major diameter"])
+        data_0d_center = DataCalculated('center', data=[np.atleast_1d(x), np.atleast_1d(y)], labels=['X center', 'Y center'])
+
+        data.append(data_0d_diameter)
+        data.append(data_0d_center)
+
+        self.dte_signal.emit(data)
+    
+if __name__ == '__main__':
+    main(__file__)
